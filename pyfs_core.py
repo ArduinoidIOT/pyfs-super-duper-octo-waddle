a254f0ea73ffdb9bb70a90e91389c1b16a6f9e2e
***** *** <rohroexperiment@rohroexperiment.com>
2019-11-16 18:30:20 +0530

    finish fsv1

--- pyfs_core.py
+++ pyfs_core.py
@@ -11,9 +11,10 @@ class FileSystem(object):
         self.nullfs = {
             "meta": {'pyfs-version': __version__},
             "contents": {
-                'name': fsname,
-                'subdirs': [],
-                'files': []
+                'name': '',
+                'type': 'dir',
+                'files': [
+                ]
             }
         }
         if not posixpath.exists(self.filename):
@@ -21,6 +22,162 @@ class FileSystem(object):
                 filesystem.write(json.dumps(self.nullfs))
                 self.fs = self.nullfs
         else:
-            with open(self.filename) as fsdata:
-                self.fs = self.json_decoder.decode(fsdata.read())
+            with open(self.filename) as filesystem:
+                self.fs = self.json_decoder.decode(filesystem.read())
 
+    def update_fs(self):
+        with open(self.filename, 'w+') as filesystem:
+            filesystem.write(json.dumps(self.fs))
+
+    def exists(self, abspath):
+        return self.isFile(abspath) or self.isDir(abspath)
+
+    def isFile(self, abspath):
+        path = abspath.strip('/').split('/')
+        contents = self.fs['contents']['files']
+        for j in range(len(path)):
+            for i in contents:
+                if j == len(path) - 1:
+                    return i['type'] == 'file' and path[j] == i['name']
+                else:
+                    if path[j] == i['name'] and i['type'] == 'dir':
+                        contents = i['files']
+                        break
+        return False
+
+    def isDir(self, abspath):
+        path = abspath.strip('/').split('/')
+        contents = self.fs['contents']['files']
+        for j in range(len(path)):
+            for i in contents:
+                if path[j] == i['name'] and i['type'] == 'dir':
+                    if j == len(path) - 1:
+                        return True
+                    else:
+                        contents = i['files']
+                        break
+        return False
+
+    def mkdir(self, dirname, location):
+        data = {
+            'name': dirname,
+            'type': 'dir',
+            'files': []
+        }
+        if location == '/':
+            root = self.fs['contents']['files']
+            for i in root:
+                if i['name'] == dirname:
+                    return 1
+            root.append(data)
+            return 0
+        path = location.strip('/').split('/')
+        root = self.fs['contents']['files']
+        for j in path:
+            for i in root:
+                if j == i['name'] and i['type'] == 'dir':
+                    root = i['files']
+                    break
+            else:
+                return 1
+        for i in root:
+            if i['name'] == dirname:
+                return 1
+        root.append(data)
+        return 0
+
+    def mkfile(self, filename, location):
+        data = {
+            'name': filename,
+            'type': 'file',
+            'contents': ''
+        }
+        if location == '/':
+            root = self.fs['contents']['files']
+            for i in root:
+                if i['name'] == filename:
+                    return 1
+            root.append(data)
+            return 0
+        path = location.strip('/').split('/')
+        root = self.fs['contents']['files']
+        for j in path:
+            for i in root:
+                if j == i['name'] and i['type'] == 'dir':
+                    root = i['files']
+                    break
+            else:
+                return 1
+        for i in root:
+            if i['name'] == filename:
+                return 1
+        root.append(data)
+        return 0
+
+    def rm(self, file, location):
+        root = self.fs['contents']['files']
+        if location != '/':
+            path = location.strip('/').split('/')
+            for j in path:
+                for i in root:
+                    if j == i['name'] and i['type'] == 'dir':
+                        root = i['files']
+                        break
+                else:
+                    return 1
+        for i in range(len(root)):
+            if root[i]['name'] == file:
+                del root[i]
+                return 0
+        else:
+            return 1
+
+    def ls(self, location):
+        root = self.fs['contents']['files']
+        if location != '/':
+            path = location.strip('/').split('/')
+            for j in path:
+                for i in root:
+                    if j == i['name'] and i['type'] == 'dir':
+                        root = i['files']
+                        break
+                else:
+                    return 1
+        out = []
+        for i in root:
+            out.append({'name': i['name'], 'type': i['type']})
+        return out
+
+    def readFile(self,location,file):
+        root = self.fs['contents']['files']
+        if location != '/':
+            path = location.strip('/').split('/')
+            for j in path:
+                for i in root:
+                    if j == i['name'] and i['type'] == 'dir':
+                        root = i['files']
+                        break
+                else:
+                    return 1
+        for i in root:
+            if i['name'] == file:
+                return i['contents']
+        else:
+            return 1
+    def writeFile(self,location,file,data):
+        root = self.fs['contents']['files']
+        if location != '/':
+            path = location.strip('/').split('/')
+            for j in path:
+                for i in root:
+                    if j == i['name'] and i['type'] == 'dir':
+                        root = i['files']
+                        break
+                else:
+                    return 1
+        for i in root:
+            if i['name'] == file:
+                i['contents'] = data
+                return 0
+        else:
+            return 1
